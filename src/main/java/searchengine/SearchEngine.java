5c4bf3e1d8028c8bfc78622aebdc88a2d5e28ae0
David Nikolaj Vinje <davv@itu.dk>
2021-12-12 19:13:25 +0100

    small cleanup

--- src/main/java/searchengine/SearchEngine.java
+++ src/main/java/searchengine/SearchEngine.java
@@ -2,11 +2,7 @@ package searchengine;
 
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 import com.sun.net.httpserver.HttpExchange;
 
