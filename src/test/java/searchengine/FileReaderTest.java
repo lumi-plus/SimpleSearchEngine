032d392d8ad64d44b35328f8f57d11f3d5d0fa0b
LUMI <lmig@itu.dk>
2021-12-15 13:27:25 +0100

    FileReaderTest

--- src/test/java/searchengine/FileReaderTest.java
+++ src/test/java/searchengine/FileReaderTest.java
@@ -1,6 +1,5 @@
 package searchengine;
 
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.io.IOException;
@@ -13,31 +12,29 @@ import org.junit.jupiter.api.TestInstance.Lifecycle;
 @TestInstance(Lifecycle.PER_CLASS)
 public class FileReaderTest {
     private FileReader fileReader;
+
     @BeforeAll
     public void setUp() {
         try {
-            //var filename = "data/test-file.txt";
-            //String filename = Files.readString(Paths.get("config.txt")).strip();
+            // var filename = "data/test-file.txt";
+            // String filename = Files.readString(Paths.get("config.txt")).strip();
             fileReader = new FileReader("data/enwiki-tiny.txt");
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-
     // @Test
     // public void testGetFile() {
-    //     assertno
-    //     assertNotNull(fileReader.getFile("enwiki-tiny.txt"));
-    //     // assertNull(fileReader.getFile("inappropriate/filepath"));
+    // assertno
+    // assertNotNull(fileReader.getFile("enwiki-tiny.txt"));
+    // // assertNull(fileReader.getFile("inappropriate/filepath"));
     // }
-
-
 
     @Test
     public void testGetPages() {
         int size = fileReader.getPages().size();
         assertEquals(6, size);
     }
-    
+
 }
